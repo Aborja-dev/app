bd3d3843709928ff0e1de6f54220e3c972059199
ankahara <hecatombe100@gmail.com>
2024-04-23 20:47:25 -0600

    animar las hero card

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,25 +4,19 @@ export const Hero = () => {
 
     return (
         <div className="grid grid-cols-4 gap-2 auto-rows-auto pt-10 h-[550px] ">
-            <div className="col-span-2 row-span-2  bg-cover bg-no-repeat relative" 
-            style={{backgroundImage: 'url(https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)'}}>
-                <div className="flex flex-col justify-center p-5 gap-3 w-3/4 absolute top-1/3 z-20">    
-                    <h3 className="text-lg font-bold tracking-wide text-gray-300 italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, corrupti!</h3>
-                    <h2 className="text-[3rem] font-thin text-white leading-tight" >Lorem ipsum dolor sit amet.</h2>
-                    <button className=" w-[150px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Ver mas</button>
-                </div>
-                <div className="w-full h-full bg-black opacity-50"></div>
+            <div className="col-span-2 row-span-2 relative overflow-hidden">
+                <HeroCard large />
             </div>
-            <div className="col-span-1 row-span-1">
+            <div className="col-span-1 row-span-1 relative overflow-hidden">
                 <HeroCard/>
             </div>
-            <div className="col-span-1 row-span-1 ">
+            <div className="col-span-1 row-span-1 relative overflow-hidden">
                 <HeroCard/>
             </div>
-            <div className="col-span-1 row-span-1 ">
+            <div className="col-span-1 row-span-1 relative overflow-hidden">
                 <HeroCard/>
             </div>
-            <div className="col-span-1 row-span-1 ">
+            <div className="col-span-1 row-span-1 relative overflow-hidden">
                 <HeroCard/>
             </div>
         </div>
--- src/components/cards/HeroCard.tsx
+++ src/components/cards/HeroCard.tsx
@@ -1,15 +1,31 @@
-export const HeroCard = () => {
-
+export const HeroCard = ({large = false}) => {
+    const textClass = {
+        large: {
+            title: "text-[3rem] font-thin text-white leading-tight",
+            subtitle: "text-lg font-bold tracking-wide text-gray-200 italic",
+            button: "w-[150px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        },
+        default: {
+            title: "text-[1.5rem] text-white leading-tight",
+            subtitle: "text-sm font-bold tracking-wide text-gray-200 italic",
+            button: "w-[100px] text-left hover:text-blue-700 text-white font-bold rounded"
+        }
+    }
+    const styles = textClass[large ? 'large' : 'default']
     return (
-        <div
-            className="w-full h-full bg-cover bg-no-repeat relative transform hover:scale-105"
-            style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)' }}>
-            <div className="flex flex-col justify-center p-5 gap-3 w-3/4 absolute z-20">
-                <h3 className="text-sm font-bold tracking-wide text-gray-200 italic">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, corrupti!</h3>
-                <h2 className="text-[1.5rem] text-white leading-tight " >Lorem ipsum dolor sit amet.</h2>
-                <button className=" w-[100px] text-left hover:text-blue-700 text-white font-bold rounded">Ver mas</button>
+        <>
+            <div
+                className="w-full h-full bg-cover bg-no-repeat absolute transform hover:scale-105 duration-200 hover:cursor-pointer"
+                style={{ backgroundImage: 'url(https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80)' }}>
+                <div className="w-full h-full bg-black opacity-50"></div>
             </div>
-            <div className="w-full h-full bg-black opacity-50"></div>
-        </div>
+            <div className={
+                `flex flex-col justify-center p-5 gap-3 w-3/4 z-20 absolute hover:cursor-pointer
+                ${large ? 'top-1/3' : 'top-0'}`}>
+                <h3 className={styles.subtitle}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, corrupti!</h3>
+                <h2 className={styles.title}>Lorem ipsum dolor sit amet.</h2>
+                <button className={styles.button}>Ver mas</button>
+            </div>
+        </>
     )
 }
